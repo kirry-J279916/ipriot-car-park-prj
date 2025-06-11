de6dbdaff3ae84551d32594d8a89b5058a84648d
j279916 <j279916@tafe.wa.edu.au>
2025-06-11 14:11:31 +0800

    Added sensors and display constructors

--- src/display.py
+++ src/display.py
@@ -1,2 +1,18 @@
 class Display:
-    pass
+    """
+    For displaying objects.
+    """
+    def __init__(self,
+                 id,
+                 car_park,
+                 message = "",
+                 is_on = False
+                 ):
+        self.id = id
+        self.car_park = car_park
+        self.message = message
+        self.is_on = is_on
+
+
+    def __str__(self):
+        return f"{self.id}: Display is {'on' if self.is_on else 'off'}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,24 @@
 class Sensor:
-    pass
+    """
+    Create sensors to detect cars.
+    """
+    def __init__(self,
+                 id,
+                 car_park,
+                 is_on = False,
+                 ):
+        self.id = id
+        self.car_park = car_park
+        self.is_on = is_on
+
+
+    def __str__(self):
+        return f"{self.id}: Sensor is {'on' if self.is_on else 'off'}"
+
+
+class EntrySensor(Sensor):
+    ...
+
+
+class ExitSensor(Sensor):
+    ...
